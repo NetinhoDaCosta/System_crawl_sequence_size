8c3faefc7c97d59c1dfa2eb38c8fc1060a4133cd
NetinhoDaCosta <rpdacostaneto@gmail.com>
2020-11-23 23:42:20 +0100

    cleanup

--- System_crawl_sequence_size_v02.py
+++ System_crawl_sequence_size_v02.py
@@ -217,20 +217,21 @@ class Mainwindow(qtw.QMainWindow):
             print("index is : {}".format(index))
             print("sequence_object is : {}".format(sequence_object))
             print("Folder is : {}".format(sequence_object["sequence"][0]))
-            
-            #bytes_item.setData(qtc.QTableWidgetItem())
-            #bytes_item = qtw.QTableWidgetItem()
-            #bytes_item.setData(Qt.DisplayRole,sequence_object["sequence"][3])
-            #bytes_item.setData(qtc.DisplayRole  , "iets")
-            
+
 
             self.ui.tableWidget_resultaat.setItem(index, 0, qtw.QTableWidgetItem(sequence_object["sequence"][0]))
             self.ui.tableWidget_resultaat.setItem(index, 1, qtw.QTableWidgetItem(sequence_object["sequence"][1]))
             self.ui.tableWidget_resultaat.setItem(index, 4, qtw.QTableWidgetItem(sequence_object["sequence"][3]))
             self.ui.tableWidget_resultaat.setItem(index, 3, qtw.QTableWidgetItem(sequence_object["sequence"][2]))
             row=row+1
-            
-            print(dir(qtw.QTableWidgetItem))
+
+            #print(help(self.ui.tableWidget_resultaat))
+            # small change
+
+            #bytes_item.setData(qtc.QTableWidgetItem())
+            #bytes_item = qtw.QTableWidgetItem()
+            #bytes_item.setData(Qt.DisplayRole,sequence_object["sequence"][3])
+            #bytes_item.setData(qtc.DisplayRole  , "iets")
 
 
 
